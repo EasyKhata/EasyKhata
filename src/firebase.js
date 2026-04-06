6792214cc486595de4a2fb6d4cb09c9cc0fe418d
Deepak Reddu <yasadeepakreddy@gmail.com>
2026-04-06 23:36:28 +0530

    firebase integration - phase 1

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3YbJ0f7haJ3rQj9ZDV4XKHixurJ6VPN4",
+  authDomain: "ledger-app-599cc.firebaseapp.com",
+  projectId: "ledger-app-599cc",
+  storageBucket: "ledger-app-599cc.firebasestorage.app",
+  messagingSenderId: "374078093945",
+  appId: "1:374078093945:web:5db233b446a9f808a5f2a8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
