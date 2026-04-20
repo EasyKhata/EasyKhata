548472fe1556400359ee3fea6c267a4e49da7c7e
Deepak Reddu <yasadeepakreddy@gmail.com>
2026-04-20 10:03:27 +0530

    performance imp

--- src/vite.config.js
+++ src/vite.config.js
@@ -6,11 +6,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/storage'],
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/storage', 'firebase/functions'],
+          capacitor: ['@capacitor/core'],
           pdf: ['jspdf'],
         }
       }
